571c7b9f714010cb1bd79e6e6105e0dbde52d55c
Benjamin Pernick <bpernick@comcast.net>
2020-02-21 12:07:54 -0600

    no close

--- db/mongoIndex.js
+++ db/mongoIndex.js
@@ -7,27 +7,22 @@ const url = 'mongodb://localhost:27017';
 // Database Name
 
 // Use connect method to connect to the server
-client.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-   
-
-});
 
 function getReviewsForListing(listing_id, order, callback) {
-  client.connect(url, function(err, client) {
+  client.connect(url, poolSize = 3000, function(err, client) {
     assert.equal(null, err);
-    const db = client.db('etsy');
-    const listings = db.collection('listings');
-    listings.find({listing_id: listing_id}).toArray((err, results) => {
-      if (err){
-        callback(err);
-      }
-      callback(null, results);
-      client.close();
-    })
+    console.log("Connected successfully to server");
+  const db = client.db('etsy');
+  const listings = db.collection('listings');
+  listings.find({listing_id: listing_id}).toArray((err, results) => {
+    if (err){
+      callback(err);
+    }
+    callback(null, results);
+  });
   })
-};
+}
+
 
 function getMoreReviews(listing_id, order, callback) {
   client.connect(url, function(err, client) {
--- db/psqlIndex.js
+++ db/psqlIndex.js
@@ -13,22 +13,23 @@ pool.connect()
 
 
 function getReviewsForListing(listing_id, order, callback) {
-  let qryStr = `SELECT 
-  b.*, 
-  a.reviews_count,
-  a.reviews_for_item,
-  c.image_url,
-  a.listing_id,
-  a.title
-FROM listings a 
-LEFT JOIN feedback b 
-  ON a.user_id = b.user_id
-LEFT JOIN (SELECT * FROM images WHERE listing_id = '${listing_id}' LIMIT 1) as c
-  ON a.listing_id = c.listing_id
-WHERE a.listing_id = '${listing_id}'
-ORDER BY b.reviewDate ${order}
-LIMIT 4
-;`;
+let qryStr = `SELECT * FROM listings WHERE listing_id = 1`
+  //   let qryStr = `SELECT 
+//   b.*, 
+//   a.reviews_count,
+//   a.reviews_for_item,
+//   c.image_url,
+//   a.listing_id,
+//   a.title
+// FROM listings a 
+// LEFT JOIN feedback b 
+//   ON a.user_id = b.user_id
+// LEFT JOIN (SELECT * FROM images WHERE listing_id = '${listing_id}' LIMIT 1) as c
+//   ON a.listing_id = c.listing_id
+// WHERE a.listing_id = '${listing_id}'
+// ORDER BY b.reviewDate ${order}
+// LIMIT 4
+// ;`;
 
   pool.query(qryStr, (err, data) => {
     err ? callback(err, null) : callback(null, data);
